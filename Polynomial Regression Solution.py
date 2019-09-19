f5801a296bc96a4d37889e7e5539653d3d7e91e6
Saranyu Ngaosri <39403998+ouujj@users.noreply.github.com>
2019-09-20 01:22:06 +0700

    Add files via upload

--- Polynomial Regression Solution.py
+++ Polynomial Regression Solution.py
@@ -1,21 +1,54 @@
 import math
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 X = [float(x) for x in input("Enter set x value split by ',': ").split(',')]
 Y = [float(y) for y in input("Enter set y value split by ',': ").split(',')]
-degree = float(input("Enter degree: "))*2
+degree = int(input("Enter degree: "))
+A=[]
 def polyRegSol():
+    
     sumxn=[] #[n,sum x, sum x^2 ,sum x^3 ...... sum x^2n ]
     sumxn.append(len(X))
     sumxn.append(sum(X))
-    sumy=(sum(Y))
+
+   
+       
+       
     
     i=2
-    while(i<=degree):
-        sumxn.append(sum([i**x for x in X]))
+    while(i<=degree*2):
+        sumxn.append(sum([x**i for x in X]))
         i+=1
+    #print(sumxn)
+    print()
+    #print("XtX :")   
+    XTX=[[] for i in range((degree+1))]
+    for i in range((degree+1)):
+        for j in range((degree+1)):
+            XTX[i].append(sumxn[j+i])
+        #print(XTX[i])
+
         
-    print(sumxn)   
+    #print("XtY :")
+    XTY=[]
+    XTY.append(sum(Y))
+    i=1
+    while(i<=degree):
+        XTY.append(sum([(X[j]**i)*Y[j] for j in range(len(X))]))
+        i+=1
+    #print(XTY)
+    print("Polynomial Matrix:")
+    for i in range(degree+1):
+       A.append("A"+str(i))
+       print(XTX[i],"  ",A[i],"  ",XTY[i])
+       
+
+
+
+
+
+
+
 
 
 
